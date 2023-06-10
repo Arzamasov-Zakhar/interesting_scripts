b2f3071f35be617fd35e1f827d770606e8b74edc
Arzamasov-Zakhar <arzamasov_z@mail.ru>
2023-06-10 13:51:26 +0300

    start romancalc

---
+++ RomanCalculator.py
@@ -0,0 +1,36 @@
+def translator(romannum: str) -> [int, str]:
+    """Калькулятор перевода из римских чисел в арабские в десятичной СИ"""
+    try:
+        if type(romannum) is not str:
+            raise TypeError
+
+        single_translator: dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        subtractively: dict = {"II": 2, "IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD": 400, "CM": 900}
+        number: int = 0
+        flag: bool = True
+
+        for index in range(len(romannum) - 1):
+            if flag:
+                if romannum[index] == "I":
+                    number += subtractively[romannum[index] + romannum[index + 1]]
+                    flag = False
+
+                elif (romannum[index] == "X" and romannum[index + 1] == "L") or \
+                        (romannum[index] == "X" and romannum[index + 1] == "C") or \
+                        (romannum[index] == "C" and romannum[index + 1] == "D") or \
+                        (romannum[index] == "C" and romannum[index + 1] == "M"):
+                    number += subtractively[romannum[index] + romannum[index + 1]]
+                    flag = False
+
+                else:
+                    number += single_translator[romannum[index]]
+            else:
+                flag = True
+        if flag:
+            number += 1
+
+        return number
+    except (KeyError, TypeError):
+        return "Введите число, записанное римскими цифрами в формате 'MDCLXVI' "
+
+
